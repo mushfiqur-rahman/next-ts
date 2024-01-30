3b0127411a4f8b243dbbe7c49652a79e50e78860
mushfiqur-rahman <mushfiq.style@gmail.com>
2024-01-30 21:26:34 +0600

    routing

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,37 +1,13 @@
 import Link from "next/link";
 import React from "react";
+import UserTable from "./UserTable";
 
-interface User {
-  id: number;
-  name: string;
-  email: string;
-}
-
-const UsersPage = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
-    cache: "no-store",
-  });
-  const users: User[] = await res.json();
-
+const UsersPage = () => {
   return (
     <>
       <h1>Users</h1>
-      <table className="table table-bordered">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <UserTable />
+
       <Link href="/" className="text-3xl text-green-500 mt-5">
         ⬅️
       </Link>
